0013c976004e513cf5361043a35c302ec426e08a
kcdhopkins <kcdhopkins@gmail.com>
2022-10-30 17:04:34 -0500

    new link

--- src/pingService/ping.js
+++ src/pingService/ping.js
@@ -3,11 +3,11 @@ import axios from "axios";
 const ping = async () => {
   try {
     if(!localStorage.getItem("key")){
-        const { data } = await axios.get("http://localhost:3000/sitepings");
+        const { data } = await axios.get("https://blue-envy-api.herokuapp.com/sitePings");
         localStorage.setItem("key", data.key)
         return
     }
-    await axios.get("http://localhost:3000/sitepings", {params: {key: localStorage.getItem("key")}}); 
+    await axios.get("https://blue-envy-api.herokuapp.com/sitePings", {params: {key: localStorage.getItem("key")}}); 
   } catch(err) {
 
   }
